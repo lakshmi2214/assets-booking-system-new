cb564f632bb1d9205fc3acf713e048698a113f9e
lakshmi <youremail@example.com>
2026-02-24 12:31:55 +0530

    Cleanup for frontend move

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.assetbooking.app',
-  appName: 'Assets Booking',
-  webDir: 'build'
-};
-
-export default config;
